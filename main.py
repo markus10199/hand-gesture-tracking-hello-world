38213ffbc04548d605459e050b7db11df950bcaa
markus10199 <77944324+markus10199@users.noreply.github.com>
2022-02-25 14:36:38 -0500

    Both pygame and cam window close together

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-#############
+# # # # # # #
 # imports
-#############
+# # # # # # #
 
 import cv2
 import pygame
 
 from HandDetector import HandDetector
+
+# # # # # # #
+# Global Vars
+# # # # # # #
+
+# Define the size of the game window
+WIDTH = 1200
+HEIGHT = 800
+# make the game window object
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+# name the game window
+pygame.display.set_caption("Best game")
+
+
+
+
 
 #########
 # main function
@@ -14,9 +30,12 @@ from HandDetector import HandDetector
 def main():
     # make a hand detector
     handDetector = HandDetector()
+    # keeps track if pygame should keep running
+    gameIsRunning = True
 
     # while the opencv window is running
-    while handDetector.shouldClose == False:
+    while not handDetector.shouldClose and gameIsRunning:
+
         # update the webcam feed and hand tracker calculations
         handDetector.update()
 
@@ -24,6 +43,14 @@ def main():
         # print out the landmark positions
         if len(handDetector.landmarkDictionary) > 0: 
             print(handDetector.landmarkDictionary[0])
+
+
+        # for all the game events
+        for event in pygame.event.get():
+             # if the game is exited out of, then stop running the game
+            if event.type == pygame.QUIT:
+                gameIsRunning = False
+
 
     # Closes all the frames
     cv2.destroyAllWindows()
